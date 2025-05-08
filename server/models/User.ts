05019ec6d19d32ad7db1afe5deafec1c2a634332
= <ishan.professional01@gmail.com>
2025-05-08 13:05:24 +0530

    create user model

---
+++ server/models/User.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema: Schema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, unique: true },
+  password: { type: String, required: true, trim: true },
+  is_verified: { type: Boolean, default: false },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+export default UserModel;
